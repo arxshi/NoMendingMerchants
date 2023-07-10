d250b59a1be933d1bfcf87fccd9a4e6fc7ce76c7
arashi <kairbektamerlan@gmail.com>
2023-07-10 16:15:32 +0600

    register commands and listeners

--- src/main/java/me/arahis/nomendingmerchants/NoMendingMerchants.java
+++ src/main/java/me/arahis/nomendingmerchants/NoMendingMerchants.java
@@ -1,13 +1,34 @@
 package me.arahis.nomendingmerchants;
 
+import me.arahis.nomendingmerchants.commands.MendingCoreCommand;
+import me.arahis.nomendingmerchants.commands.subcommands.OffCommand;
+import me.arahis.nomendingmerchants.commands.subcommands.OnCommand;
+import me.arahis.nomendingmerchants.listeners.VillagerAcquireTradeListener;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class NoMendingMerchants extends JavaPlugin {
 
+    private static NoMendingMerchants plugin;
+    private MendingCoreCommand coreCommand;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
 
+        getConfig().options().copyDefaults();
+        saveConfig();
+
+        plugin = this;
+        coreCommand = new MendingCoreCommand();
+
+        getCommand("mending").setExecutor(new MendingCoreCommand());
+
+        Bukkit.getPluginManager().registerEvents(new VillagerAcquireTradeListener(), this);
+
+    }
+
+    public static NoMendingMerchants getPlugin() {
+        return plugin;
     }
 
     @Override
